33af7cbf832fb5ef894e7aa81461b13a5ee653e2
soyboy <sbvegan@protonmail.com>
2022-05-01 11:29:51 -0700

    wip: VRF stuff

--- raffle-app-solidity-nextjs/contracts/Raffle.sol
+++ raffle-app-solidity-nextjs/contracts/Raffle.sol
@@ -2,10 +2,14 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
+error Raffle__UpkeepNotNeeded();
 
-contract Raffle {
+contract Raffle is KeeperCompatibleInterface {
     enum RaffleState {
         Open,
         Calculating
@@ -15,10 +19,32 @@ contract Raffle {
 
     RaffleState public s_raffleState;
     uint256 public immutable i_entranceFee;
+    uint256 public immutable i_interval;
+    uint256 public s_lastTimestamp;
     address payable[] public s_players;
+    VRFCoordinatorV2Interface public immutable i_vrfCoordinator;
+    bytes32 public i_gasLane;
+    uint64 public i_subscriptionId;
+    uint32 public i_callbackGasLimit;
 
-    constructor(uint256 entranceFee) {
+    uint16 public constant REQUEST_CONFIRMATIONS = 3;
+    uint32 public constant NUM_WORDS = 1;
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinatorV2,
+        bytes32 gasLane, // keyhash
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimestamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -33,5 +59,32 @@ contract Raffle {
         // you can enter
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
+    }
+
+    // When should we pick a winner?
+    function checkUpkeep(
+        bytes memory /** checkData */
+    )
+        public
+        view
+        returns (
+            bool upkeepNeeded,
+            bytes memory /** performData */
+        )
+    {
+        bool isOpen = RaffleState.Open == s_raffleState;
+        bool timePassed = ((block.timestamp - s_lastTimestamp) > i_interval);
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
+        return (upkeepNeeded, "0x0");
+    }
+
+    function performUpkeep(bytes calldata) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded();
+        }
+        s_raffleState = RaffleState.Calculating;
     }
 }
